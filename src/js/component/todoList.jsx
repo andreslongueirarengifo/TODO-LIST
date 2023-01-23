21b7d3b2c14cb2939631ef93817a6185e3697f8e
Andres Longueira <andreslongueirarengifo@gmail.com>
2023-01-23 16:20:43 +0000

    Show deleteButton if the task is active

--- src/js/component/todoList.jsx
+++ src/js/component/todoList.jsx
@@ -6,13 +6,13 @@ import Button from 'react-bootstrap/Button';
 function TodoList(prop) {
 
 
-
     const HandleRemoveTask=(index)=>{
         prop.setter(prop.list.filter((e,currentIndex)=>index!=currentIndex)) 
     }
 
+
     const list = prop.list.map((e,i)=>{
-            return (<ListGroup.Item className='d-flex justify-content-between'>{e} <Button variant="danger" onClick={()=>HandleRemoveTask(i)}><i class="fa-solid fa-trash"></i></Button> </ListGroup.Item>)
+            return (<ListGroup.Item className='d-flex justify-content-between task'>{e}<Button variant="danger" className='invisible' onClick={()=>HandleRemoveTask(i)}><i className="fa-solid fa-trash"></i></Button> </ListGroup.Item>)
         })
 
   return (
